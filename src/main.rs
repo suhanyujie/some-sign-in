0515ce79d3c33d12e2a3f27e9177db7c341b10f3
suhanyujie <suhanyujie@qq.com>
2023-03-16 10:41:45 +0800

    update doc

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::{Result as AnyResult, Ok};
+use reqwest::header::{HeaderMap, self};
 
 /// 启动一个定时任务服务
 /// 每天执行签到请求 todo
@@ -6,13 +7,22 @@ use anyhow::{Result as AnyResult, Ok};
 
 #[tokio::main]
 async fn main() {
-    req_sign_in();
+    req_sign_in().await;
 }
 
 async fn req_sign_in() ->AnyResult<()> {
-    let sign_url = "https://glados.rocks/console";
+    let sign_url = "https://glados.rocks/api/user/checkin";
     let resp = reqwest::get(sign_url).await?;
     println!("{:?}", resp);
     Ok(())
+}
+
+async fn post(url: &str) ->AnyResult<String> {
+    let client = reqwest::Client::new();
+    let mut headers = HeaderMap::new();
+    headers.insert("Content-Type", "application/json".parse().unwrap());
+    let resp = client.post(url).headers(headers).send().await?.bytes().await?;
+    let respStr = String::from_utf8_lossy(&resp);
+    Ok(respStr.to_string())
 }
 
